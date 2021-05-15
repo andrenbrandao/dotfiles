6d58819e75263bd99ece7d1ed4296f9c18712882
André Brandão <andrenbrandao@gmail.com>
2021-05-15 10:01:16 -0300

    chore: add video2gif to aliases.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -8,3 +8,8 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 # neofetch
 alias neofetch='neofetch --colors 212 159 212 212 --ascii_colors 103 212 103 --color_blocks off'
+
+# ffmpeg video to gif
+function video2gif() {
+  ffmpeg -i $1 -r 15 -vf "scale=632:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -ss 00:00:00 $2
+}
