36b9d8f4a2386a79d6e304a8c4741e76fd3258e8
André Brandão <andrenbrandao@gmail.com>
2021-06-28 11:33:52 -0300

    chore: remove professional email

--- .aliases.zsh
+++ .aliases.zsh
@@ -13,3 +13,6 @@ alias neofetch='neofetch --colors 212 159 212 212 --ascii_colors 103 212 103 --c
 function video2gif() {
   ffmpeg -i $1 -r 15 -vf "scale=632:-1,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -ss 00:00:00 $2
 }
+
+# git
+alias gprunesquashmerged='git checkout -q master && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base master $branch) && [[ $(git cherry master $(git commit-tree $(git rev-parse $branch^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done'
