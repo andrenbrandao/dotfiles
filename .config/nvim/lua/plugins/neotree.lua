bbbefc47a1ca5f0da44ec4cd6108b4292c409beb
André Brandão <andrenbrandao@gmail.com>
2024-01-26 14:44:57 -0300

    chore(nvim): add none-ls

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -3,7 +3,10 @@ return {
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
+    "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-  }
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', {})
+  end
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,8 +3,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', {})
+    vim.keymap.set('n', '<leader>sf', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
   end
 }
