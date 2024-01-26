071b7ee9ab845248044e3029133fa89498ca486b
André Brandão <andrenbrandao@gmail.com>
2024-01-26 20:20:40 -0300

    chore(nvim): add new plugins

---
+++ .config/nvim/lua/plugins/autocompletions.lua
@@ -0,0 +1,73 @@
+return {
+  -- {
+  -- 	"L3MON4D3/LuaSnip",
+  -- 	lazy = false,
+  -- 	dependencies = {
+  -- 		"saadparwaiz1/cmp_luasnip",
+  -- 		"rafamadriz/friendly-snippets",
+  -- 	},
+  -- 	config = function()
+  -- 		require("luasnip.loaders.from_vscode").lazy_load()
+  -- 	end,
+  -- },
+  -- {
+  --   -- Autocompletion
+  --   'hrsh7th/nvim-cmp',
+  --   dependencies = {
+  --     -- Snippet Engine & its associated nvim-cmp source
+  --     'L3MON4D3/LuaSnip',
+  --     'saadparwaiz1/cmp_luasnip',
+  --
+  --     -- Adds LSP completion capabilities
+  --     'hrsh7th/cmp-nvim-lsp',
+  --     'hrsh7th/cmp-path',
+  --
+  --     -- Adds a number of user-friendly snippets
+  --     'rafamadriz/friendly-snippets',
+  --   },
+  -- }
+  --
+
+  -- {
+  -- 	"hrsh7th/nvim-cmp",
+  -- 	dependencies = {
+  -- 		"hrsh7th/cmp-nvim-lsp",
+  -- 		"L3MON4D3/LuaSnip",
+  -- 		"saadparwaiz1/cmp_luasnip",
+  -- 		"rafamadriz/friendly-snippets",
+  -- 	},
+  -- 	event = { "InsertEnter", "CmdlineEnter" },
+  -- 	config = function()
+  -- 		local cmp = require("cmp")
+  -- 		local luasnip = require("luasnip")
+  --
+  -- 		require("luasnip.loaders.from_vscode").lazy_load()
+  -- 		luasnip.config.setup({})
+  --
+  -- 		cmp.setup({
+  -- 			snippet = {
+  -- 				expand = function(args)
+  -- 					luasnip.lsp_expand(args.body)
+  -- 				end,
+  -- 			},
+  -- 			window = {
+  -- 				completion = cmp.config.window.bordered(),
+  -- 				documentation = cmp.config.window.bordered(),
+  -- 			},
+  -- 			mapping = cmp.mapping.preset.insert({
+  -- 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+  -- 				["<C-f>"] = cmp.mapping.scroll_docs(4),
+  -- 				["<C-s"] = cmp.mapping.complete(),
+  -- 				["<C-e>"] = cmp.mapping.abort(),
+  -- 				["<CR>"] = cmp.mapping.confirm({ select = true }),
+  -- 			}),
+  -- 			sources = cmp.config.sources({
+  -- 				{ name = "luasnip", keyword_length = 2 }, -- For luasnip users.
+  -- 				{ name = "nvim_lsp" },
+  -- 			}, {
+  -- 				{ name = "buffer" },
+  -- 			}),
+  -- 		})
+  -- 	end,
+  -- },
+}
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,10 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+      auto_install = true,
       sync_install = false,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
   end
 }
