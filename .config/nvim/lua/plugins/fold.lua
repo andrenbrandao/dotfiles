e7d84fce2839683c0364f8deb41a3773b6ddb693
André Brandão <andrenbrandao@gmail.com>
2024-01-27 20:06:34 -0300

    feat(nvim): add fold plugin

---
+++ .config/nvim/lua/plugins/fold.lua
@@ -0,0 +1,12 @@
+-- Autofold Plugin
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	init = function()
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+	end,
+	config = function()
+		require("ufo").setup({})
+	end,
+}
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -46,10 +46,10 @@ return {
 
 			-- Global mappings.
 			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+			vim.keymap.set("n", "<space>de", vim.diagnostic.open_float)
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-			vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+			vim.keymap.set("n", "<space>dq", vim.diagnostic.setloclist)
 
 			-- Use LspAttach autocommand to only map the following keys
 			-- after the language server attaches to the current buffer
