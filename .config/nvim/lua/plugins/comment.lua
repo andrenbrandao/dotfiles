d92a2256e94b61402daf7fe84958e06de92382b0
André Brandão <andrenbrandao@gmail.com>
2024-01-27 15:12:30 -0300

    feat(nvim): update keybindings for comment

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,2 +1,20 @@
 -- "gc" to comment visual regions/lines
-return { "numToStr/Comment.nvim", opts = {} }
+return {
+	"numToStr/Comment.nvim",
+	opts = {},
+	config = function()
+		require("Comment").setup()
+		local api = require("Comment.api")
+
+		-- Toggle current line (linewise) using C-/
+		vim.keymap.set("n", "<C-_>", api.toggle.linewise.current)
+
+		local esc = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+
+		-- Toggle selection (linewise) using C-/
+		vim.keymap.set("x", "<C-_>", function()
+			vim.api.nvim_feedkeys(esc, "nx", false)
+			api.toggle.linewise(vim.fn.visualmode())
+		end)
+	end,
+}
