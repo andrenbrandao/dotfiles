6fde0ea0374f86e178479922af2773db91cd80df
André Brandão <andrenbrandao@gmail.com>
2024-01-27 16:24:22 -0300

    feat(nvim): add new keymaps

--- .config/nvim/lua/vim-settings.lua
+++ .config/nvim/lua/vim-settings.lua
@@ -9,9 +9,20 @@ vim.o.shiftwidth = 2
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- Save undo history
+-- save undo history
 vim.o.undofile = true
 
--- Case-insensitive searching UNLESS \C or capital in search
+-- case-insensitive searching UNLESS \C or capital in search
 vim.o.ignorecase = true
 vim.o.smartcase = true
+
+-- move visual lines up or down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- keeps cursor on the beginning when joining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- center when going down or up
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
