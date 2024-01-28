ca846737648cc113b376136bef2e9ccaad7580f0
André Brandão <andrenbrandao@gmail.com>
2024-01-28 01:20:23 -0300

    feat: stow X11 config

--- stow-files.sh
+++ stow-files.sh
@@ -10,10 +10,18 @@ fi
 STOW_FOLDERS=$STOW_FOLDERS DOTFILES=$DOTFILES
 
 pushd $DOTFILES
+
+# stow folders
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
     echo "stow $folder"
     stow -D $folder
     stow $folder
 done
+
+# stow X11 config
+echo "stow X11"
+sudo stow -D X11 --target=/ -v
+sudo stow X11 --target=/ -v
+
 popd
