875d87606766bfd1ef8a8300fb1e87f6eead5256
André Brandão <andrenbrandao@gmail.com>
2024-01-27 23:14:34 -0300

    feat: remove dotfiles alias

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -3,9 +3,6 @@
 # fix external monitor with black screen
 alias mfix="xrandr --output DP-1 --mode 1024x768 && sleep 3 && xrandr --output DP-1 --auto --left-of eDP-1"
 
-# dotfiles alias for bare git repo
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
 # neofetch
 alias neofetch='neofetch --colors 212 159 212 212 --ascii_colors 103 212 103 --color_blocks off'
 
