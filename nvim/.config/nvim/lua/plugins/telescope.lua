2d788f4c6d5fafc4959f622de65edf2d9a5e3c54
André Brandão <andrenbrandao@gmail.com>
2024-02-02 06:43:46 -0300

    feat(nvim): search git_files but fallback to search_files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -16,7 +16,29 @@ return {
 
 			local builtin = require("telescope.builtin")
 
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+			-- # fallback to find_files if git_files can't find a .git directory #
+			-- We cache the results of "git rev-parse"
+			-- Process creation is expensive in Windows, so this reduces latency
+			local is_inside_work_tree = {}
+
+			local project_files = function()
+				local opts = {} -- define here if you want to define something
+
+				local cwd = vim.fn.getcwd()
+				if cwd ~= nil and is_inside_work_tree[cwd] == nil then
+					vim.fn.system("git rev-parse --is-inside-work-tree")
+					is_inside_work_tree[cwd] = vim.v.shell_error == 0
+				end
+
+				if is_inside_work_tree[cwd] then
+					builtin.git_files(opts)
+				else
+					builtin.find_files(opts)
+				end
+			end
+			-- # end of fallback #
+
+			vim.keymap.set("n", "<leader>sf", project_files, {})
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
