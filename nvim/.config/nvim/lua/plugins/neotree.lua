5f05655d5cf200d706dcf762a7b6d46d8d3ee6d9
André Brandão <andrenbrandao@gmail.com>
2024-03-02 12:16:50 -0300

    feat(nvim): follow current file on neotree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -8,5 +8,14 @@ return {
   },
   config = function()
     vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<cr>", {})
+
+    require("neo-tree").setup({
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      filesystem = {
+        follow_current_file = { -- Open current file on the sidebar
+          enabled = true,
+        },
+      },
+    })
   end,
 }
