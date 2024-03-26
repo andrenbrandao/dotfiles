5a9ce23020946db7c07294498236182aeeb3323c
André Brandão <andrenbrandao@gmail.com>
2024-03-26 04:36:42 -0300

    feat(nvim): add gdb dap support

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -33,6 +33,29 @@ return {
         dapui.close()
       end
 
+      -- GDB config
+      -- needs to have at least gdb 14.1 to have DAP support
+      dap.adapters.gdb = {
+        type = "executable",
+        command = "gdb",
+        args = { "-i", "dap" },
+      }
+
+      for _, language in ipairs({ "c", "cpp" }) do
+        dap.configurations[language] = {
+          {
+            name = "Launch",
+            type = "gdb",
+            request = "launch",
+            program = function()
+              return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            end,
+            cwd = "${workspaceFolder}",
+            stopAtBeginningOfMainSubprogram = false,
+          },
+        }
+      end
+
       dap.adapters["pwa-node"] = {
         type = "server",
         host = "localhost",
