fc8d42bf0347f07e370022a25265aa54f9dc9da4
André Brandão <andrenbrandao@gmail.com>
2024-04-16 05:17:00 -0300

    fix(nvim): cpp debug terminal

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -47,6 +47,7 @@ return {
             name = "Launch",
             type = "gdb",
             request = "launch",
+            console = "integratedTerminal",
             program = function()
               return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
             end,
