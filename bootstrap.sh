c9c5fd8da3256e3af7b0677b5a54fd171c955565
André Brandão <andrenbrandao@gmail.com>
2025-08-04 20:00:19 -0300

    Remove gitconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [[ -z $STOW_FOLDERS ]]; then
-    STOW_FOLDERS="git,nvim,tmux,zsh,bin,guake,albert"
+    STOW_FOLDERS="nvim,tmux,zsh,bin,guake,albert"
 fi
 
 if [[ -z $DOTFILES ]]; then
