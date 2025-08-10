f76c8d70ac3b924a0a944ef418f46fdec0a2da9c
André Brandão <andrenbrandao@gmail.com>
2025-08-10 12:24:50 -0300

    nvim: Fix markdown plugin

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -2,7 +2,5 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+  build = ":call mkdp#util#install()",
 }
